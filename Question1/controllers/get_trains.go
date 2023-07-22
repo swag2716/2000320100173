af19288483fe194d43ae95ccb7adda7677647f70
Swapnika Agrawal <swapnikaagrawal@gmail.com>
2023-07-22 11:56:33 +0530

    Completed

--- Question1/controllers/get_trains.go
+++ Question1/controllers/get_trains.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"sort"
 	"time"
@@ -10,27 +11,78 @@ import (
 )
 
 func GetTrainsHanlder(w http.ResponseWriter, r *http.Request) {
-	var filteredTrains []models.Train
+
+	access_token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTAwMDcwODAsImNvbXBhbnlOYW1lIjoiRXhwcmVzcyBUcmFpbnMiLCJjbGllbnRJRCI6IjEzZjU1N2M1LWJmMmMtNGY2NS1iNjdhLWY3YzhjZTUwNmFkZCIsIm93bmVyTmFtZSI6IiIsIm93bmVyRW1haWwiOiIiLCJyb2xsTm8iOiIyMDAwMzIwMTAwMTczIn0.hRgx-u9byxzmVcv_PMhDypmi7xOiTN3SrwC4KXLs4tE"
+
+	apiURL := "http://20.244.56.144/train/trains"
+	req, err := http.NewRequest("GET", apiURL, nil)
+
+	if err != nil {
+		http.Error(w, "Failed to create API request", http.StatusInternalServerError)
+		return
+	}
+	req.Header.Add("access_token", "Bearer "+access_token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		http.Error(w, "Failed to fetch data from API", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Failed to fetch data from API", http.StatusInternalServerError)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read data from API", http.StatusInternalServerError)
+		return
+	}
+
+	print(string(body))
+
+	var trains []models.Train
+	err = json.Unmarshal(body, &trains)
+	if err != nil {
+		http.Error(w, "Failed to parse data from API", http.StatusInternalServerError)
+		return
+	}
 
 	now := time.Now()
 	next30Mins := now.Add(time.Minute * 30)
 	next12Hours := now.Add(time.Hour * 12)
-	for _, train := range models.MockTrains {
-		if train.Departure.After(next30Mins) && train.Departure.Before(next12Hours) {
+	var filteredTrains []models.Train
+	for _, train := range trains {
+		departureTime := time.Date(now.Year(), now.Month(), now.Day(), train.DepartureTime.Hours, train.DepartureTime.Minutes, train.DepartureTime.Seconds, 0, now.Location())
+		if departureTime.After(next30Mins) && departureTime.Before(next12Hours) {
 			filteredTrains = append(filteredTrains, train)
 		}
 	}
 
 	sort.SliceStable(filteredTrains, func(i, j int) bool {
-		if filteredTrains[i].Coaches[1].Price != filteredTrains[j].Coaches[1].Price {
-			return filteredTrains[i].Coaches[1].Price < filteredTrains[j].Coaches[1].Price
+		if filteredTrains[i].Price["AC"] != filteredTrains[j].Price["AC"] {
+			return filteredTrains[i].Price["AC"] < filteredTrains[j].Price["AC"]
 		}
-		if filteredTrains[i].Coaches[0].SeatAvailability != filteredTrains[j].Coaches[0].SeatAvailability {
-			return filteredTrains[i].Coaches[0].SeatAvailability > filteredTrains[j].Coaches[0].SeatAvailability
+		if filteredTrains[i].SeatsAvailable["sleeper"] != filteredTrains[j].SeatsAvailable["sleeper"] {
+			return filteredTrains[i].SeatsAvailable["sleeper"] > filteredTrains[j].SeatsAvailable["sleeper"]
 		}
-		return filteredTrains[i].Departure.After(filteredTrains[j].Departure)
+		return compareTimePoints(filteredTrains[i].DepartureTime, filteredTrains[j].DepartureTime)
 	})
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(filteredTrains)
+}
+
+func compareTimePoints(tp1, tp2 models.TimePoint) bool {
+	if tp1.Hours != tp2.Hours {
+		return tp1.Hours < tp2.Hours
+	}
+	if tp1.Minutes != tp2.Minutes {
+		return tp1.Minutes < tp2.Minutes
+	}
+	return tp1.Seconds < tp2.Seconds
 }
--- Question1/models/train.go
+++ Question1/models/train.go
@@ -1,70 +1,15 @@
 package models
 
-import "time"
-
-type Coach struct {
-	Type             string  `json:"type"`
-	SeatAvailability int     `json:"seatAvailability"`
-	Price            float64 `json:"price"`
+type TimePoint struct {
+	Hours   int `json:"hours"`
+	Minutes int `json:"minutes"`
+	Seconds int `json:"seconds"`
 }
 
 type Train struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Departure time.Time `json:"departure"`
-	Coaches   []Coach   `json:"coaches"`
-}
-
-var MockTrains = []Train{
-	{
-		ID:        1,
-		Name:      "Express 1",
-		Departure: time.Now().Add(time.Minute * 15),
-		Coaches: []Coach{
-			{
-				Type:             "sleeper",
-				SeatAvailability: 100,
-				Price:            500.0,
-			},
-			{
-				Type:             "ac",
-				SeatAvailability: 50,
-				Price:            1000.0,
-			},
-		},
-	},
-	{
-		ID:        2,
-		Name:      "Super Express 2",
-		Departure: time.Now().Add(time.Minute * 40),
-		Coaches: []Coach{
-			{
-				Type:             "sleeper",
-				SeatAvailability: 100,
-				Price:            1000.0,
-			},
-			{
-				Type:             "ac",
-				SeatAvailability: 30,
-				Price:            1500.0,
-			},
-		},
-	},
-	{
-		ID:        3,
-		Name:      "Express 3",
-		Departure: time.Now().Add(time.Hour * 10),
-		Coaches: []Coach{
-			{
-				Type:             "sleeper",
-				SeatAvailability: 60,
-				Price:            500.0,
-			},
-			{
-				Type:             "ac",
-				SeatAvailability: 20,
-				Price:            1000.0,
-			},
-		},
-	},
+	TrainNumber    int            `json:"trainNumber"`
+	TrainName      string         `json:"trainName"`
+	DepartureTime  TimePoint      `json:"departureTime"`
+	SeatsAvailable map[string]int `json:"seatsAvailable"` // Key: coach type (e.g., "sleeper" or "ac"), Value: number of available seats
+	Price          map[string]int `json:"price"`
 }
